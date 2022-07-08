19a2432ea72b596bbee15f74a636a872228b0503
axemc <192119741@qq.com>
2022-07-08 23:56:42 +0800

    update

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -1,15 +1,11 @@
 // intro1.rs
-// About this `I AM NOT DONE` thing:
 // We sometimes encourage you to keep trying things on a given exercise, even
 // after you already figured it out. If you got everything working and feel
-// ready for the next exercise, remove the `I AM NOT DONE` comment below.
 // Execute the command `rustlings hint intro1` for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    println!("Hello and");
-    println!(r#"       welcome to...                      "#);
+    println!("Hello {}! welcome to...", "Axemc");
     println!(r#"                 _   _ _                  "#);
     println!(r#"  _ __ _   _ ___| |_| (_)_ __   __ _ ___  "#);
     println!(r#" | '__| | | / __| __| | | '_ \ / _` / __| "#);
